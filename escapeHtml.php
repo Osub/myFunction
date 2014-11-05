11cf75d44261e159a041c75c6984cb364f17380d
Osub <814566123@qq.com>
2014-11-05 13:55:17 +0800

    Create escapeHtml.php

---
+++ escapeHtml.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*返回过滤转义后的html
+* Returns HTML escaped variable
+*
+* @access	public
+* @param	mixed
+* @return	mixed
+*/
+if ( ! function_exists('html_escape'))
+{
+	function html_escape($var)
+	{
+		if (is_array($var))
+		{
+			return array_map('html_escape', $var);
+		}
+		else
+		{
+			return htmlspecialchars($var, ENT_QUOTES, config_item('charset'));
+		}
+	}
+}
