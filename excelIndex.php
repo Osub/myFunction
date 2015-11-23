27233d6c098306975d97e7a793a4a5d99877e156
Osub <814566123@qq.com>
2015-11-23 14:10:56 +0800

    Update excelIndex.php

--- excelIndex.php
+++ excelIndex.php
@@ -1,17 +1,18 @@
 # myFunction
+<?php
 //我的函数库
 //生成excel表头索引
 //生成excel索引  $i==0
-	public function indexXY($i){
-		if($i>25){
-			$num = ceil($i/25);
-			$index = '';
-			for($j=1;$j<$num;$j++){
-				$index .= chr($j-1+65);
-			}
-			$index .= chr(($i-($num-1)*26)+65);
-		}else{
-			$index = chr($i+65);
+public function indexXY($i){
+	if($i>25){
+		$num = ceil($i/25);
+		$index = '';
+		for($j=1;$j<$num;$j++){
+			$index .= chr($j-1+65);
 		}
-		return $index;
+		$index .= chr(($i-($num-1)*26)+65);
+	}else{
+		$index = chr($i+65);
 	}
+	return $index;
+}
